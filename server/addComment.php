87532b45d04364939c1b952056436fe594ae191e
lordsivan <siva@neelsoftware.com>
2013-09-28 19:48:24 +0530

    Update addComment.php

--- server/addComment.php
+++ server/addComment.php
@@ -1,3 +1,16 @@
 <?php
 
+$phpobj=json_decode($_POST['json'], false); 
+$con=mysqli_connect("localhost","testuser","testpass","citizen");
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+$queryStr="UPDATE issue SET status='Closed' WHERE id='".$phpobj->id."'";
+print_r($queryStr);
+mysqli_query($con,$queryStr);
+
+mysqli_close($con);
+
 ?>

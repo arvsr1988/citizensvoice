8d36ee063c17d23b4b0288eb886915032c727c68
lordsivan <siva@neelsoftware.com>
2013-09-28 17:09:49 +0530

    Update resolve.php

--- server/resolve.php
+++ server/resolve.php
@@ -1,3 +1,15 @@
 <?php
+$con=mysqli_connect("localhost","testuser","testpass","citizen");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+$data= $_POST['json'];
+$phpobj = json_decode($data,true);
 
+mysqli_query($con,"UPDATE issue SET status="Closed"
+WHERE id='"+$phpobj->id+"'");
+
+mysqli_close($con);
 ?>

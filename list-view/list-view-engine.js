f5f3f422f365473ece818dd1557dca071b124b66
SharmiladeviNS <sharmiladevi17@gmail.com>
2013-09-28 21:01:15 +0530

    list view started

---
+++ list-view/list-view-engine.js
@@ -0,0 +1,16 @@
+﻿function listIssues() {
+    var json = "[{ \"issueType\": \"Street Light\", \"location\": \"Vadapalani\", \"time\": \"5 hour ago\", \"imageUrl\": \"\", \"status\": \"Open\", \"votingcount\": \"1\", \"id\": \"1\", \"ImageUrl\": \"image\/test.jpg\" }, { \"issueType\": \"Street Light\", \"location\": \"Vadapalani\", \"time\": \"5 hour ago\", \"imageUrl\": \"\", \"status\": \"Open\", \"votingcount\": \"1\", \"id\": \"1\", \"ImageUrl\": \"image\/test.jpg\"}]";
+    return JSON.parse(json);
+    //    var string = { location: "All", issuetype: "All" };
+    //    var dataString = "json=" + JSON.stringify(string);
+    //    $.ajax({
+    //        type: 'POST',
+    //        url: 'http://ec2-54-242-157-231.compute-1.amazonaws.com/citizensvoice/trunk/server/getListIssues.php',
+    //        data: dataString,
+    //        dataType: 'json',
+    //        success: function (data) {
+    //            alert(data);
+    //        }
+    //    });
+}
+ 
--- list-view/list-view.js
+++ list-view/list-view.js
@@ -1,1 +1,26 @@
-﻿
+﻿function testButton() {
+    alert("testing");
+}
+function getListIssues() {
+    var issuesList = listIssues();
+    $(function () {
+        $.each(issuesList, function (list) {
+            $("#list").append('<img src="http://ec2-54-242-157-231.compute-1.amazonaws.com/image/test.jpg" />');
+            $("#list").append('<label id="location-label"/>');
+        });
+
+    });
+}
+ <table><tr>
+         <a id="issue-type" href="">issueType</a>
+        <label id="location-label">
+            location</label>
+        <label id="time">
+            time</label>
+        <img src="" />
+        <label id="status-label">
+            status</label>
+        <label id="votes-label">
+            Voting Count</label>
+         </tr>
+        </table>

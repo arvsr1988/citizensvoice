5ea5d7f5ce40c34ffb7a669d8b377466c8d718b7
lordsivan <siva@neelsoftware.com>
2013-09-28 18:28:11 +0530

    Update resolve.php

--- server/resolve.php
+++ server/resolve.php
@@ -7,7 +7,8 @@ if (mysqli_connect_errno())
   }
 $phpobj=json_decode($_POST['json'], false); 
 print_r($phpobj->id);
-mysqli_query($con,"UPDATE issue SET status='Closed' WHERE id='"+$phpobj->id+"'");
+$queryStr="UPDATE issue SET status='Closed' WHERE id='"+$phpobj->id+"'";
+mysqli_query($con,$queryStr);
 
 mysqli_close($con);
 ?>

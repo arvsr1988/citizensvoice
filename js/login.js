18ebd2b3a3f0edf8b2edf57e615c94e6520ca172
arvind <arvsr2002@gmail.com>
2013-09-28 19:26:20 +0530

    add issue form and login page

---
+++ js/login.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	$("body").on("click", ".login", function(){
+		login.displayLoginDialog($(this).prop("href"));
+		return false;
+		});
+	});
+	
+var login = {
+	displayLoginDialog : function(redirectPage){
+		var redirectUrl = redirectPage;
+		bootbox.prompt("Login with your email address", function(result) {                
+		  if (result !== null && result !== "" && IsEmail(result)) {
+			cookieUtils.setCookie(appConstants.loginCookie, result, 10);
+			alert("cookie set as " + login.getLoggedInUser());
+			if(redirectUrl !== null && redirectUrl !== "" ){
+				window.location.href=redirectUrl;
+			}
+		  } else {
+			alert("Enter a valid email address");
+			}
+		});
+	},
+	getLoggedInUser : function(){
+		return cookieUtils.getCookie(appConstants.loginCookie);
+	}
+	
+}
+
+function IsEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
---
+++ js/utils/cookieUtils.js
@@ -0,0 +1,28 @@
+//@Arvind - shamelessly copied from http://www.quirksmode.org/js/cookies.html
+var cookieUtils = {
+	getCookie : function(name){
+			var nameEQ = name + "=";
+			var ca = document.cookie.split(';');
+			for(var i=0;i < ca.length;i++) {
+				var c = ca[i];
+				while (c.charAt(0)==' ') c = c.substring(1,c.length);
+				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+			}
+			return null;
+		},
+	
+	setCookie : function(name,value,days){
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			var expires = "; expires="+date.toGMTString();
+		}
+		else var expires = "";
+		document.cookie = name+"="+value+expires+"; path=/";
+		},
+		
+	eraseCookie : function (name) {
+	createCookie(name,"",-1);
+}
+	
+}

543d4ceeab07302e7867baed9a4d247eb54bee95
arvind <arvsr2002@gmail.com>
2013-09-29 17:05:43 +0530

    issue list screen ui change

--- js/appConstants.js
+++ js/appConstants.js
@@ -1,5 +1,7 @@
 var appConstants = {
 		loginCookie : "login",
-		serverUrl : "http://ec2-54-235-60-155.compute-1.amazonaws.com/citizensvoice/trunk/server/",
+		serverUrl : "http://ec2-54-235-60-155.compute-1.amazonaws.com/citizensvoice/trunk/server/",	
+		imageRoot : "http://ec2-54-235-60-155.compute-1.amazonaws.com", 	
 		issueIdParam : "id"
 }
+//

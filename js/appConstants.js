edf493743271e11203398377b758e47ccf8d81de
arvind <arvsr2002@gmail.com>
2013-09-29 15:35:13 +0530

    new server url

--- js/appConstants.js
+++ js/appConstants.js
@@ -1,5 +1,5 @@
 var appConstants = {
 		loginCookie : "login",
-		serverUrl : "http://ec2-174-129-171-156.compute-1.amazonaws.com/citizensvoice/trunk/server/",
+		serverUrl : "http://ec2-54-235-60-155.compute-1.amazonaws.com/citizensvoice/trunk/server/",
 		issueIdParam : "id"
 }
